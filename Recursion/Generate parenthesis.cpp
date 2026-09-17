d2bc2f0e96102913695fd9491b6b0ef87fcd8636
Tonmoy3078 <tkgaming3078@gmail.com>
2026-09-17 10:52:39 +0600

    Recursion

---
+++ Recursion/Generate parenthesis.cpp
@@ -0,0 +1,87 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int,int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef double dl;
+
+#define endl '\n'
+#define PB push_back
+#define F first
+#define S second
+#define all(a) (a).begin(),(a).end()
+#define rall(a) (a).rbegin(),(a).rend()
+#define sz(x) (int)x.size()
+
+const double PI = acos(-1);
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+#define MOD 1000000007
+
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define sqr(a) ((a) * (a))
+
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+#define file() freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+
+#define dbg(args...) do {cerr << #args << " : "; faltu(args); } while(0)
+void faltu () {            cerr << endl;}
+template < typename T, typename ... hello>void faltu( T arg, const hello &... rest) {cerr << arg << ' ';faltu(rest...);}
+
+ll gcd ( ll a, ll b ) { return __gcd ( a, b ); }
+ll lcm ( ll a, ll b ) { return a * ( b / gcd ( a, b ) ); }
+vector<string>valid;
+void generate(string &s,int open,int close)
+{
+    if(open== 0 && close ==0)
+    {
+        valid.push_back(s);
+        return ;
+    }
+    if(open>0)
+    {
+        s.push_back('(');
+        generate(s,open-1,close);
+        s.pop_back();///It is backtracking;
+    }
+
+    if(close>0)
+    {
+        if(open<close)
+        {
+            s.push_back(')');
+            generate(s,open,close-1);
+            s.pop_back();///It is backtracking;
+        }
+    }
+
+
+}
+int main()
+{
+    optimize();
+
+    int n;
+    cin>>n;
+
+    string s;
+    generate(s,n,n);
+
+    for(auto u : valid)
+    {
+        cout<<u<<" ";
+    }
+
+    cout<<endl;
+}
